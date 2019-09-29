2bb9f961c9850e4b2af8a09ef90c52840133aa4f
Jasmine Kim <jas89922@gmail.com>
2019-09-29 22:09:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   food_for_dinner = ["pasta", "steak", "fish", "salad", "chicken"]
-  food_for_dinner << "chicken"
+  food_for_dinner.push "chicken"
   end
 
 def add_element_to_start_of_array(array, element)
