e6d95bdeb1e96dc23b031ee53fc47f6b7a7156cd
Jasmine Kim <jas89922@gmail.com>
2019-09-29 22:13:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   food_for_dinner = ["pasta", "steak", "fish", "salad"]
-  .pop
+  pasta.pop = food_for_dinner
 end
 
 def remove_element_from_start_of_array(array)
