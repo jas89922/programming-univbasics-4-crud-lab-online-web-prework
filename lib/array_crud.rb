ecc06674049eb991168846b7df14d95d4b3cd38d
Jasmine Kim <jas89922@gmail.com>
2019-09-29 22:33:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array[3]
+  array[pasta]
 end
 
 def retrieve_first_element_from_array(array)
@@ -31,7 +31,7 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  array[1]
+  array[salad]
 end
 
 def update_element_from_index(array, index_number, element)
